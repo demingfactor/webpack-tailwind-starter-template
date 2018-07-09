7d16e5df4b275755c0509355b8dbdfe816d06369
Richie Khoo <evolve2k@gmail.com>
2018-07-09 21:02:52 +0930

    Added fonts.css placeholder file.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // require('typeface-montserrat')
+import './assets/stylesheets/fonts.css'
 import './assets/stylesheets/lighter-youtube.css'
 import './assets/javascripts/lighter-youtube.js'
 import './styles.css'
 import './assets/stylesheets/variables.css'
 import './assets/stylesheets/identity.css'
-import './assets/stylesheets/animations.css'
 import './assets/stylesheets/layout.css'
+import './assets/stylesheets/animations.css'
 import './assets/stylesheets/functions.css'
