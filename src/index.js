2b1841959f8458da38465353b11500beec7b2a1e
Richie Khoo <evolve2k@gmail.com>
2018-08-06 14:17:47 +0930

    Now using CSS @import (as js import wasn’t respecting sort order). Moved Tailwind into two CSS files.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 // require('typeface-montserrat')
-import './assets/stylesheets/fonts.css'
-import './assets/stylesheets/lighter-youtube.css'
 import './assets/javascripts/lighter-youtube.js'
-import './styles.css'
-import './assets/stylesheets/variables.css'
-import './assets/stylesheets/identity.css'
-import './assets/stylesheets/layout.css'
-import './assets/stylesheets/animations.css'
-import './assets/stylesheets/functions.css'
+import './assets/stylesheets/app.css'
